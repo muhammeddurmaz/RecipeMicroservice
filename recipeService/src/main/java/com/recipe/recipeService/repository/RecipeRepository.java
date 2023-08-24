bfe5da3ce53fd0dc14ac0adbb85a48147b8c7bb8
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-08-24 22:19:18 +0300

    RecipeRepository added

---
+++ recipeService/src/main/java/com/recipe/recipeService/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.recipe.recipeService.repository;
+
+import com.recipe.recipeService.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
