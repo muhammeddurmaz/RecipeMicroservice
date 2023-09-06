0446a3c9688880274c92805f276e8faa8f269114
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-09-06 23:15:07 +0300

    RecipeIngredient added

---
+++ ingredient-service/src/main/java/com/recipe/ingredientservice/repository/RecipeIngredientRepository.java
@@ -0,0 +1,9 @@
+package com.recipe.ingredientservice.repository;
+
+import com.recipe.ingredientservice.model.RecipeIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+}
