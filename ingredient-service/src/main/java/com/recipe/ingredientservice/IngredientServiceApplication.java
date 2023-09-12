0348ab9078cab33be171eaeee584196b5271864a
muhammeddurmaz <muhammed.durmaz@derinarge.com>
2023-09-12 20:23:23 +0300

    eureka and transactional added

--- ingredient-service/src/main/java/com/recipe/ingredientservice/IngredientServiceApplication.java
+++ ingredient-service/src/main/java/com/recipe/ingredientservice/IngredientServiceApplication.java
@@ -2,8 +2,10 @@ package com.recipe.ingredientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class IngredientServiceApplication {
 
 	public static void main(String[] args) {
--- ingredient-service/src/main/java/com/recipe/ingredientservice/service/impl/RecipeIngredientServiceImpl.java
+++ ingredient-service/src/main/java/com/recipe/ingredientservice/service/impl/RecipeIngredientServiceImpl.java
@@ -5,17 +5,18 @@ import com.recipe.ingredientservice.dto.IngredientDTO;
 import com.recipe.ingredientservice.dto.RecipeIngredientDTO;
 import com.recipe.ingredientservice.dto.ViewRecipeIngredientRequest;
 import com.recipe.ingredientservice.dto.mapper.RecipeIngredientMapper;
-import com.recipe.ingredientservice.model.RecipeIngredient;
 import com.recipe.ingredientservice.repository.RecipeIngredientRepository;
 import com.recipe.ingredientservice.service.IngredientService;
 import com.recipe.ingredientservice.service.RecipeIngredientService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class RecipeIngredientServiceImpl implements RecipeIngredientService {
 
     private final IngredientService ingredientService;
@@ -30,6 +31,7 @@ public class RecipeIngredientServiceImpl implements RecipeIngredientService {
     @Override
     public ViewRecipeIngredientRequest saveRecipeIngredient(CreateRecipeIngredientRequest request) {
         ViewRecipeIngredientRequest viewRecipeIngredientRequest = new ViewRecipeIngredientRequest();
+        viewRecipeIngredientRequest.setIngredients(new HashMap<String ,String>());
         HashMap<Long,String> ingredients = request.getIngredients();
 
         for(Long id : ingredients.keySet()){
@@ -50,12 +52,14 @@ public class RecipeIngredientServiceImpl implements RecipeIngredientService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public ViewRecipeIngredientRequest getRecipeIngredients(Long recipeId) {
         List<RecipeIngredientDTO> recipeIngredients =  recipeIngredientRepository.findByRecipeId(recipeId)
                 .stream()
                 .map(recipeIngredientMapper::toDto).collect(Collectors.toList());
 
         ViewRecipeIngredientRequest viewRecipeIngredientRequest = new ViewRecipeIngredientRequest();
+        viewRecipeIngredientRequest.setIngredients(new HashMap<String,String>());
         recipeIngredients.forEach(dto -> viewRecipeIngredientRequest.getIngredients().put(dto.getIngredient().getName(),dto.getAmount()));
         viewRecipeIngredientRequest.setRecipeId(recipeId);
 
